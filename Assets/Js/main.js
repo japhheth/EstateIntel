1494eb0b8ff6d6d052ca4e93b0e70b3db56ad580
japhheth <japhheth@gmail.com>
2019-12-20 16:43:47 +0100

    Added my comments

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -1,15 +1,22 @@
 let tableBody = '';
 
+//OnLoad function
+
 $(function(){
+
+    //Fetches the data
     $.ajax({
         url:"https://70c5b72c-65db-4a66-ba01-3e14763157e8.mock.pstmn.io/",
         type:"GET",
         contentType:"application/json",
         success:function(response){
-        
             let data = response.data;
+
+            //Loops through the data and renders the table
             for(let i = 0; i < data.length; i++){
                 let status = "";
+
+                //Checks the status of the projects and renders a new badge/
                 if(data[i].status == "On hold")
                 {
                     status = '<span class="badge badge-primary status-bg">On Hold</span>'
@@ -39,13 +46,14 @@ $(function(){
 
                 }
 
+                //Pushes the data in a tabe body.
                 tableBody += `<tr><td>${data[i].project_name}</td><td>${data[i].developer}</td><td>${data[i].main_contractor}</td><td>${data[i].lga}</td><td>${data[i].state}</td><td>${status}</td><td>${data[i].sector}</td></tr>`
 
-
-
-            }
+            };
+            //Displays the table body
             $('#tableBody').html(tableBody);
+            
             $('#estateTable').DataTable();
         }
-    })
-})
+    });
+});
